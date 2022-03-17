dc0308b322d85fbe64da893c4dd76b2ab459c2a7
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-03-18 00:00:54 +0100

    GOHTTPSERVER _QRCODE

---
+++ html/js/core.js
@@ -0,0 +1,20 @@
+const sendbutton = document.querySelector('#BTNSEND');
+sendbutton.addEventListener("click", ()=>{
+    const inputstr= document.querySelector('#mystr').value
+    if (inputstr === ""){return}
+    request =new  XMLHttpRequest()
+    const requestdata = `stringtoencode=${encodeURIComponent(inputstr)}`
+    request.open('POST',"/createQRCODE", true)
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    request.responseType = 'blob'
+    request.onload = async (e)=>{
+     
+      
+      const objectimg = document.querySelector("#QRIMAGE") 
+      objectimg.src = URL.createObjectURL(request.response)
+      
+
+   }
+    request.send(requestdata)
+
+})
